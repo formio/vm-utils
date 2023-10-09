6a1cee088f97537ddced9381a07433cf14643bf8
Alexey Nikipelau <alexey.nikipelau@form.io>
2023-10-09 16:23:17 +0300

    added isolate factory

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-module.exports = require('./src/vmUtil');
+const ivm = require('isolated-vm');
+
+const DEFAULT_MEMORY_LIMIT = 16;
+
+function newIsolate(memoryLimit) {
+  return new ivm.Isolate({
+    memoryLimit: memoryLimit |
+                 process.env.VM_MEMORY_LIMIT |
+                 DEFAULT_MEMORY_LIMIT
+  });
+}
+
+module.exports = {
+  newIsolate,
+  ...require('./src/vmUtil')
+}
