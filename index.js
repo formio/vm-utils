8ae0889b0b01bebe672e685c50bfb2dd74106dce
Alexey Nikipelau <alexey.nikipelau@form.io>
2023-11-06 17:09:25 +0300

    exports updated

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
-  ...require('./src/get-isolate'),
-  ...require('./src/isolate-cache'),
-  ...require('./src/context-builder'),
-  ...require('./src/vm-util')
+  getIsolate: require('./src/get-isolate'),
+  isolateCache: require('./src/isolate-cache'),
+  ContextBuilder: require('./src/context-builder'),
+  ...require('./src/transfer')
 }
